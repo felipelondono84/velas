00bff61dc18835178d65b20a20bdc84c6c17bb2e
Felipe Londono Guarin <felipelondono@MacBook-Pro-de-Felipe.local>
2025-03-12 18:54:23 -0500

    Agregados nuevos

--- Modulos/php_conexion.php
+++ Modulos/php_conexion.php
@@ -1,10 +1,17 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+
+$conexion = mysqli_connect($host, $user, $password, $database);
 	
 	#$conexion = mysql_connect("localhost","root","");
 	#mysql_select_db("prueba",$conexion);
 
-$conexion = mysqli_connect("localhost","lflsoftw_uservel","h9H[~*AlZOv-","lflsoftw_velas");
+//$conexion = mysqli_connect("localhost","lflsoftw_uservel","h9H[~*AlZOv-","lflsoftw_velas");
 
 
 	date_default_timezone_set("America/Bogota");
