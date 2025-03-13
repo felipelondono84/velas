7917fd2d1178b0bf1b68361f55b846315471e4c1
Felipe Londono Guarin <felipelondono@MacBook-Pro-de-Felipe.local>
2025-03-12 20:12:42 -0500

    Hola

--- Modulos/php_conexion.php
+++ Modulos/php_conexion.php
@@ -5,7 +5,7 @@ $DB_USER=$_ENV["DB_USER"];
 $DB_PASSWORD=$_ENV["DB_PASSWORD"];
 $DB_NAME=$_ENV["DB_NAME"];
 $DB_PORT=$_ENV["DB_PORT"];
-$db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
+$conexion=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
 
 
 // $host = getenv('DB_HOST');
